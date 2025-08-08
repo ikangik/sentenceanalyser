697e1b0f6c35b013f0b15eae340359092657bf93
ikangik <ikang@plc.nsw.edu.au>
2025-08-08 14:07:41 +1000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css'; // You may or may not have this file
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
